cc37ac2826c6c069a55593202263192628dc15a3
Peter Brandon <peterkbrandon@gmail.com>
2017-09-24 22:20:44 -0400

    First working solution

--- knight.rb
+++ knight.rb
@@ -2,9 +2,98 @@ require 'matrix'
 
 class Knight
 
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
   def knight_moves(xy1,xy2)
+  	node = xy_to_node(xy1)
+  	node_que = []
+  	visited = []
+    node_que << node
+ 
+    until node_que.empty?           
+      node = node_que.shift 
+      visited << node
+      puts print_node(node)
+	  if match?(node,xy2)
+	  	puts "Match!"
+	    return print_path(node)
+	  else 
+	  	if node.nxyr && !visited.include?(node.nxyr)
+	  		node_que << node.nxyr
+	  		node.nxyr.parent_node = node
+	  	end
+	  	if node.nxyl && !visited.include?(node.nxyl)
+	  		node_que << node.nxyl
+	  		node.nxyl.parent_node = node
+	  	end
+	  	if node.sxyr && !visited.include?(node.sxyr)
+	  		node_que << node.sxyr
+	  		node.sxyr.parent_node = node
+	  	end
+	  	if node.sxyl && !visited.include?(node.sxyl)
+	  		node_que << node.sxyl
+	  		node.sxyl.parent_node = node
+	  	end
+	  	if node.exyr && !visited.include?(node.exyr)
+	  		node_que << node.exyr
+	  		node.exyr.parent_node = node
+	  	end
+	  	if node.exyl && !visited.include?(node.exyl)
+	  		node_que << node.exyl
+	  		node.exyl.parent_node = node
+	  	end
+	  	if node.wxyr && !visited.include?(node.wxyr)
+	  		node_que << node.wxyr
+	  		node.wxyr.parent_node = node
+	  	end
+	  	if node.wxyl && !visited.include?(node.wxyl)
+	  		node_que << node.wxyl
+	  		node.wxyl.parent_node = node
+	  	end
+	  end
+    end
+    return nil
+  end
+
+  def print_node(node)
+  	return "#{node.xy}"
+  end
+
+  def xy_to_node(xy)
+  	return @board[xy[0],xy[1]]
+  end
+
+  def print_path(node)
+  	counter = 1
+  	path=[]
+  	path << node
+  	until node.parent_node == nil
+  		counter += 1
+  		path << node.parent_node
+  		node = node.parent_node
+  	end
+  	path = path.reverse
+  	puts "You made it in #{counter} moves: "
+    path.each {|x| print "#{x.xy} "}
+  	return 
+  end
+
+  def print_que(node_que)
+  	node_que.each {|x| print "#{x.xy} "}
+  end
 
 
+  def match?(xy,xy2)
+  	puts "testing #{print_node(xy)} and #{xy2}"
+  	if  xy == xy_to_node(xy2)
+  		return true
+  	else
+  	  return false
+  	end
   end
 
 end #class end
@@ -13,7 +102,7 @@ class GameBoard
   attr_accessor :matrix
 
   def initialize 
-    @matrix = Matrix.build(8,8) {|row,col| Node.new([row,col])}
+    @matrix = Matrix.build(7,7) {|row,col| Node.new([row,col])}
     assign_moves(@matrix)
   end
 
@@ -59,7 +148,7 @@ class GameBoard
   def verify_coordinates(xy)
   	x = xy[0]
   	y = xy[1]
-	if x < 0 || x > 8 || y < 0 || y > 8
+	if x < 0 || x > 7 || y < 0 || y > 7
 		return false
 	else
 		return true
@@ -70,8 +159,7 @@ class GameBoard
 end #class end
 
 class Node
-  attr_accessor :xy, :nxyl, :nxyr, :sxyl, :sxyr, :exyl, :exyr, :wxyl, :wxyr
-
+  attr_accessor :xy, :nxyl, :nxyr, :sxyl, :sxyr, :exyl, :exyr, :wxyl, :wxyr, :parent_node
   def initialize(xy,nxyl=nil,nxyr=nil,sxyl=nil,sxyr=nil,exyl=nil,exyr=nil,wxyl=nil,wxyr=nil)
     @xy = xy
 	@nxyl = nxyl
@@ -95,4 +183,6 @@ end #class end
 chess = GameBoard.new
 chess.matrix[0,1].print_node
 puts chess.matrix[4,4].sxyr
+knight = Knight.new(chess.matrix)
+knight.knight_moves([0,0],[4,6])
 
